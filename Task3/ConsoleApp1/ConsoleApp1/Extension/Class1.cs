91eb3fb5e6d78bd8265b90a38f9bd53edd1c6f96
AqayevNicat <nijatam@code.edu.az>
2021-12-19 14:11:58 +0400

    19.12.2021

---
+++ Task3/ConsoleApp1/ConsoleApp1/Extension/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Extension
+{
+    static class Class1
+    {
+        public static void Met(this string word, string part)
+        {
+            bool res = false;
+
+
+            #region 1-ci usul
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int a = 0; a <= word.Length - i; a++)
+                {
+                    if (word.Substring(i, a) == part)
+                    {
+                        res = true;
+                    }
+                }
+            }
+            #endregion 1ci usulun sonu
+
+
+
+            #region 2ci usul
+            //string[] bos = new string[0];
+            //int s = 1;
+
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    for (int a = 0; a < word.Length - i; a++)
+            //    {
+            //        Array.Resize(ref bos, s += i);
+            //        bos[i] += word[a + i];
+
+            //        if (bos[i] == part)
+            //        {
+            //            res = true;
+            //        }
+            //    }
+            //}
+            #endregion 2ci usulun sonu
+
+
+
+            if (res)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+           
+        }
+    }
+}
+
---
+++ Task3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1.Extension;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello World".Met("Hell");
+
+        }
+    }
+}
